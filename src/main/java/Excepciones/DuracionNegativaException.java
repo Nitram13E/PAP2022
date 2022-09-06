c43ba49bed31a02b2cb4a4b9718e79c6134bba3f
AXA37251 <diego.poblete.cl@gmail.com>
2022-09-06 12:32:13 -0300

    Se agregan excepciones para CActDeportiva, se cambia persistencia a update, se agregan nuevas excepciones para duración y costo

---
+++ src/main/java/Excepciones/DuracionNegativaException.java
@@ -0,0 +1,14 @@
+package Excepciones;
+
+import java.io.Serial;
+
+public class duracionMenorQueCeroException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public duracionMenorQueCeroException(String string){
+        super(string);
+    }
+}
+
+
